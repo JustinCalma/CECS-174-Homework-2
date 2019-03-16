56be273a6e369c3349f5ca888d2a6bac8fa8b865
Justin Calma <calma.justin.00@gmail.com>
2019-03-16 13:08:05 -0700

    CECS 174 Homework 2

---
+++ JustinCalma_Homework2.py
@@ -0,0 +1,25 @@
+def menu():
+    print("\n 1.- Check a Palindrome")
+    print("2.- Check a Word Square")
+    print("Quit")
+
+    a = input("Select a function")
+    run = False
+    counter = 0
+    while run == False
+        for char.isdigit() == False:
+            print("ERROR: Enter a number between 1 and 3")
+            a = input("Enter a number")
+            break
+        counter = counter + 1
+    if counter == len(a):
+        run = True
+    decision = int(a)
+
+    if decision == 1:
+        #check_Palindrome()
+    elif decision == 2:
+        #check_Word_Square()
+    elif decision == 3:
+        #print("Goodbye...")
+        #quit()
